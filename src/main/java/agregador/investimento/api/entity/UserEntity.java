749fa9c368385900ae1d058e054a05eec6582c21
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-03-01 15:48:47 -0300

    UserEntity criada

---
+++ src/main/java/agregador/investimento/api/entity/UserEntity.java
@@ -0,0 +1,45 @@
+package agregador.investimento.api.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Email(message = "Indique um email válido")
+    private String email;
+
+    @Length(min = 8, message = "Indique uma senha com no minímo 8 caractéres")
+    private String password;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+    
+}
