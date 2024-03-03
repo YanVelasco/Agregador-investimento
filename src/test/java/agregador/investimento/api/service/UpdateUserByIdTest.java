d82679952d25054f8940eb37e5eae8c48a9db1c5
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-03-03 16:15:15 -0300

    Teste para atualizar o usuário pelo ID

--- src/test/java/agregador/investimento/api/service/UpdateUserByIdTest.java
+++ src/test/java/agregador/investimento/api/service/UpdateUserByIdTest.java
@@ -1,12 +1,26 @@
 package agregador.investimento.api.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import agregador.investimento.api.dto.UpdateUserDTO;
+import agregador.investimento.api.entity.UserEntity;
 import agregador.investimento.api.repository.UserRepository;
 
-public class UpdateUserByIdTest {
+@ExtendWith(MockitoExtension.class)
+class UpdateUserByIdTest {
 
     @Mock
     private UserRepository userRepository;
@@ -14,8 +28,38 @@ public class UpdateUserByIdTest {
     @InjectMocks
     private UpdateUserById updateUserById;
 
+    @SuppressWarnings("null")
     @Test
-    void testExecute() {
+    @DisplayName("Should update user when user exists")
+    void testExecuteUserExists() {
+        // Arrange
+        String userId = UUID.randomUUID().toString();
+        UserEntity user = new UserEntity();
+        UpdateUserDTO updateUserDTO = new UpdateUserDTO("newName", "newPassword");
+        Mockito.when(userRepository.findById(UUID.fromString(userId))).thenReturn(Optional.of(user));
+        Mockito.when(userRepository.save(any(UserEntity.class))).thenAnswer(i -> i.getArguments()[0]);
 
+        // Act
+        UserEntity result = updateUserById.execute(userId, updateUserDTO);
+
+        // Assert
+        assertEquals("newName", result.getName());
+        assertEquals("newPassword", result.getPassword());
+        verify(userRepository, times(1)).findById(UUID.fromString(userId));
+        verify(userRepository, times(1)).save(any(UserEntity.class));
+    }
+
+    @SuppressWarnings("null")
+    @Test
+    @DisplayName("Should throw exception when user does not exist")
+    void testExecuteUserDoesNotExist() {
+        // Arrange
+        String userId = UUID.randomUUID().toString();
+        UpdateUserDTO updateUserDTO = new UpdateUserDTO("newName", "newPassword");
+        Mockito.when(userRepository.findById(UUID.fromString(userId))).thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(NoSuchElementException.class, () -> updateUserById.execute(userId, updateUserDTO));
+        verify(userRepository, times(1)).findById(UUID.fromString(userId));
     }
 }
