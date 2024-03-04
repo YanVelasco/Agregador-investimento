46f2afba7934a457eca97a16c3d55fd53b52f3a9
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-03-04 13:28:43 -0300

    Entidade BillingAdress criada e relacionada com a entidade Account

--- src/main/java/agregador/investimento/api/entity/AccountEntity.java
+++ src/main/java/agregador/investimento/api/entity/AccountEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,5 +31,9 @@ public class AccountEntity {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    @OneToOne(mappedBy = "account")
+    @PrimaryKeyJoinColumn
+    private BillingAdressEntity billingAdress;
 
 }
---
+++ src/main/java/agregador/investimento/api/entity/BillingAdressEntity.java
@@ -0,0 +1,36 @@
+package agregador.investimento.api.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "billing_adress")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingAdressEntity {
+    
+    @Id
+    @Column(name = "account_id")
+    private UUID id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "account_id")
+    private AccountEntity account;
+
+    private String street;
+
+    private String number;
+
+}
