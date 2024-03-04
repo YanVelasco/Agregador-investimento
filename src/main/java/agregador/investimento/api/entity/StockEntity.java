b908a3203cc001ba9b9282af6b5afd31293efd37
Yan_Velasco <yancarlosduartevelasco@gmail.com>
2024-03-04 13:53:51 -0300

    Entidade BillingAdress criada e relacionada com a entidade Accoun

---
+++ src/main/java/agregador/investimento/api/entity/StockEntity.java
@@ -0,0 +1,23 @@
+package agregador.investimento.api.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stock")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockEntity {
+
+    @Id
+    private String id;
+    
+    private String description;
+    
+}
